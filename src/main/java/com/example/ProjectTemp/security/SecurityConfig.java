55636ce23df291bcfbd54fb8746e3831de8b2178
Alexandr20i <Alexandr20i@yandex.ru>
2024-05-25 02:21:08 +0400

    V1.1 ничего не поменялось

--- src/main/java/com/example/ProjectTemp/security/SecurityConfig.java
+++ src/main/java/com/example/ProjectTemp/security/SecurityConfig.java
@@ -1,38 +1,90 @@
+////package com.example.ProjectTemp.security;
+////
+////import org.springframework.context.annotation.Bean;
+////import org.springframework.context.annotation.Configuration;
+////import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+////import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+////import org.springframework.security.core.userdetails.UserDetailsService;
+////import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+////import org.springframework.security.crypto.password.PasswordEncoder;
+////import org.springframework.security.web.SecurityFilterChain;
+////
+////@Configuration
+////public class SecurityConfig {
+////
+////    private final UserDetailsService userDetailsService;
+////
+////    public SecurityConfig(UserDetailsService userDetailsService) {
+////        this.userDetailsService = userDetailsService;
+////    }
+////
+////    @Bean
+////    public PasswordEncoder passwordEncoder() {
+////        return new BCryptPasswordEncoder();
+////    }
+////
+////    @Bean
+////    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+////        http
+////                .authorizeRequests(authorize -> authorize
+////                        .requestMatchers("/register", "/login", "/css/**", "/js/**").permitAll()
+////                        .anyRequest().authenticated()
+////                )
+////                .formLogin(form -> form
+////                        .loginPage("/login")
+////                        .defaultSuccessUrl("/profile", true)
+////                        .permitAll()
+////                )
+////                .logout(logout -> logout
+////                        .logoutUrl("/logout")
+////                        .logoutSuccessUrl("/login?logout")
+////                        .permitAll()
+////                );
+////
+////        return http.build();
+////    }
+////
+////    @Bean
+////    public UserDetailsService userDetailsService() {
+////        return userDetailsService;
+////    }
+////
+////    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+////        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+////    }
+////}
+//
 //package com.example.ProjectTemp.security;
 //
+//
+//import lombok.RequiredArgsConstructor;
 //import org.springframework.context.annotation.Bean;
 //import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+//import org.springframework.security.authentication.AuthenticationManager;
+//import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+//import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 //import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.core.userdetails.UserDetailsService;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 //import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 //import org.springframework.security.crypto.password.PasswordEncoder;
 //import org.springframework.security.web.SecurityFilterChain;
 //
 //@Configuration
-//public class SecurityConfig {
-//
-//    private final UserDetailsService userDetailsService;
-//
-//    public SecurityConfig(UserDetailsService userDetailsService) {
-//        this.userDetailsService = userDetailsService;
-//    }
+//@EnableWebSecurity
+//@RequiredArgsConstructor
+////@EnableGlobalMethodSecurity(prePostEnabled = true)
+//public class SecurityConfig{
 //
 //    @Bean
-//    public PasswordEncoder passwordEncoder() {
-//        return new BCryptPasswordEncoder();
-//    }
-//
-//    @Bean
-//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 //        http
 //                .authorizeRequests(authorize -> authorize
-//                        .requestMatchers("/register", "/login", "/css/**", "/js/**").permitAll()
+//                        .requestMatchers("/register", "/login", "/css/**", "/js/**", "/showusers").permitAll()
 //                        .anyRequest().authenticated()
 //                )
 //                .formLogin(form -> form
 //                        .loginPage("/login")
-//                        .defaultSuccessUrl("/profile", true)
+//                        .defaultSuccessUrl("/dashborad", true)
 //                        .permitAll()
 //                )
 //                .logout(logout -> logout
@@ -40,16 +92,15 @@
 //                        .logoutSuccessUrl("/login?logout")
 //                        .permitAll()
 //                );
-//
 //        return http.build();
+//    }
+//    @Bean
+//    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+//        return authenticationConfiguration.getAuthenticationManager();
 //    }
 //
 //    @Bean
-//    public UserDetailsService userDetailsService() {
-//        return userDetailsService;
-//    }
-//
-//    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-//        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+//    public PasswordEncoder passwordEncoder() {
+//        return new BCryptPasswordEncoder(8);
 //    }
 //}
